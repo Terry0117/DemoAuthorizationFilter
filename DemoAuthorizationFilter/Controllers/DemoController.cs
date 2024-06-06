064f80c6dd6355e15a63fbf7e350f2c633c43200
Terry0117 <jsps000106@gmail.com>
2024-06-06 18:08:45 +0800

    實作登入、登出功能

--- DemoAuthorizationFilter/Controllers/DemoController.cs
+++ DemoAuthorizationFilter/Controllers/DemoController.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace DemoAuthorizationFilter.Controllers
 {
-    //驗證使用者
+    //驗證使用者(可指定特定使用者)
     [Authorize(Users = "Alex,Bonny")]
     public class DemoController : Controller
     {
         // GET: Demo
         public ActionResult Index()
         {
-            ViewBag.Name = "陳小明";
+            ViewBag.Name = User.Identity.Name;
             return View();
         }
 
@@ -21,6 +22,33 @@ namespace DemoAuthorizationFilter.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Login(string id, string password)
+        {
+            var idArray = new string[] { "Alex", "Bonny", "Cody" };
+            var pwdArray = new string[] { "111", "222", "333" };
+
+            //檢查輸入帳密是否存在於上方陣列中
+            for (int i = 0; i < idArray.Length; i++) 
+            { 
+                if (id == idArray[i] && password == pwdArray[i])
+                {
+                    //表單驗證服務，授權指定帳號
+                    FormsAuthentication.RedirectFromLoginPage(id, true);
+                    return RedirectToAction("Index"); 
+                }
+            }
+            ViewBag.Error = "帳密驗證失敗";
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut(); //登出
+            return RedirectToAction("Login");
         }
 
     }
--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 	  <forms loginUrl="~/Demo/Login"/>
 	</authentication>
 ```
+## 指定使用者通過驗證
+FormsAuthentication.RedirectFromLoginPage("帳號",是否建立永久Cookie)
+FormsAuthentication類別使用時須引用System.Web.Security此命名空間。
