d9bdc6580e9863556f21464489fec775c7e78a59
Terry0117 <jsps000106@gmail.com>
2024-06-06 17:39:30 +0800

    加入專案檔案。

---
+++ DemoAuthorizationFilter/Controllers/DemoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoAuthorizationFilter.Controllers
+{
+    //驗證使用者
+    [Authorize(Users = "Alex,Bonny")]
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Name = "陳小明";
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
